e18025be052f1496a8725082ddebe179c4b6ab3b
Jeff-Maina <devyoungblud@gmail.com>
2024-11-05 19:32:50 +0300

    feat: customize admin panel by extending actions

--- tasksProject/tasksapp/admin.py
+++ tasksProject/tasksapp/admin.py
@@ -1,3 +1,55 @@
-from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Task
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email', 'username', 'is_staff',
+                    'is_superuser')  # Customize as needed
+    search_fields = ('email', 'username')  # Add search capability
+    ordering = ('email',)
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'priority', 'status',
+                    'deadline')  # Add fields to display
+    list_filter = ('status', 'priority')  # Add filters for easier navigation
+    # Enable searching by title and description
+    search_fields = ('title', 'description')
+    ordering = ('-deadline',)  # Order by deadline, latest first
+
+    # You can define custom actions here if needed
+    actions = ['mark_as_done', 'mark_as_in_progress',
+               'mark_as_cancelled', 'approve']
+
+    def mark_as_done(self, request, queryset):
+        queryset.update(status='Done')  # Update status to Done
+        self.message_user(request, "Selected tasks have been marked as Done.")
+
+    def mark_as_in_progress(self, request, queryset):
+        queryset.update(status='In-progress')  # Update status to In-progress
+        self.message_user(
+            request, "Selected tasks have been marked as In-progress.")
+
+    def mark_as_cancelled(self, request, queryset):
+        queryset.update(status='Cancelled')  # Update status to Cancelled
+        self.message_user(
+            request, "Selected tasks have been marked as Cancelled.")
+
+    def approve(self, request, queryset):
+        queryset.update(status='Cancelled')  # Update status to Cancelled
+        self.message_user(
+            request, "Selected tasks have been marked as Cancelled.")
+
+    mark_as_done.short_description = "Mark selected tasks as Done"
+    mark_as_in_progress.short_description = "Mark selected tasks as In-progress"
+    mark_as_cancelled.short_description = "Mark selected tasks as Cancelled"
+
+
+# Register the TaskAdmin with the Task model
+admin.site.register(Task, TaskAdmin)
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- tasksProject/tasksapp/models.py
+++ tasksProject/tasksapp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from .manager import CustomUserManager
 from django.utils import timezone
 
@@ -8,7 +8,8 @@ from django.utils import timezone
 
 
 class Task(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
     priority = models.CharField(max_length=100)
@@ -19,19 +20,19 @@ class Task(models.Model):
         return (f"{self.title},{self.description}")
 
 
- 
-class CustomUser(AbstractBaseUser):
+class CustomUser(AbstractBaseUser, PermissionsMixin):
     id = models.AutoField(primary_key=True)
     email = models.EmailField(max_length=50, unique=True)
     username = models.CharField(max_length=50)
     country = models.CharField(max_length=100)
     is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(default=timezone.now)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
     objects = CustomUserManager()
-        
+
     def __str__(self):
         return self.username
